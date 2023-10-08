de85ac23804aab873dc1c692ecd858358af9eeb2
Aleksandr Voskoboinikov <109810573+wwoskie@users.noreply.github.com>
2023-10-08 13:09:33 +0300

    Add count_aa and get_fracture_of_aa funcs

--- modules/protein_tools.py
+++ modules/protein_tools.py
@@ -230,3 +230,54 @@ def get_length_of_protein(seq: str) -> int:
     return len(seq)
 
 
+def count_aa(seq: str, aminoacids: str = None) -> dict:
+    """
+    Counts the number of given or all amino acids in a protein sequence.
+
+    Arguments:
+    - seq (str): sequence to count amino acids
+    - aminoacids (str): which amino acids to count in sequence
+
+    Return:
+    - dict: a dictionary with amino acids and its count
+    """
+
+    aa_dict_count = {}
+    if (aminoacids is None) or (aminoacids == ''):
+        '''
+        I added an additional condition for user-friendly experience.
+        E.g., we can want to find specific aminoacid, look on result and then look on all aminoacids.
+        Without this condition we have to delete keyword argument, but with it we can only make it empty.
+        '''
+        aminoacids = ''.join(set(seq))
+    for aa in aminoacids:
+        aa_dict_count[aa] = seq.count(aa)
+    return aa_dict_count
+
+
+def get_fracture_of_aa(seq: str, show_as_percentage: bool = False, aminoacids: str = None) -> dict:
+    """
+    Calculates the fracture or percentage of amino acids in a protein sequence.
+
+    Arguments:
+    - seq (str): sequence in which you need to calculate the fracture of amino acids
+    - show_as_percentage (bool): change it to True, if you want to get results with percentages
+    - aminoacids (str): the fracture of which amino acids to count in the sequence
+
+    Return:
+    - dict: a dictionary with amino acids and its fracture or percentage
+    """
+
+    if show_as_percentage:
+        mult = 100
+        round_var = 2
+    else:
+        mult = 1
+        round_var = 4
+    aa_dict_count = count_aa(seq, aminoacids=aminoacids)
+    aa_dict_percent = {}
+    len_of_protein = get_length_of_protein(seq)
+    for aa, count in aa_dict_count.items():
+        aa_dict_percent[aa] = round(count / len_of_protein * mult, round_var)
+    return aa_dict_percent
+
