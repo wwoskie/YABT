eaf5eef308dfd1901d80a312fc763b8d50153c5d
Aleksandr Voskoboinikov <109810573+wwoskie@users.noreply.github.com>
2023-10-08 11:35:35 +0300

    Add RNA_CODON_TABLE const

--- modules/protein_tools.py
+++ modules/protein_tools.py
@@ -46,3 +46,32 @@ RNA_AA_TABLE = {
     'g': ['ggu', 'ggc', 'gga', 'ggg']
 }
 
+RNA_CODON_TABLE = { 
+  'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L', 'UCU': 'S',
+  'UCC': 'S', 'UCA': 'S', 'UCG': 'S', 'UAU': 'Y', 'UAC': 'Y',
+  'UAA': '*', 'UAG': '*', 'UGU': 'C', 'UGC': 'C', 'UGA': '*',
+  'UGG': 'W', 'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+  'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P', 'CAU': 'H',
+  'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q', 'CGU': 'R', 'CGC': 'R',
+  'CGA': 'R', 'CGG': 'R', 'AUU': 'I', 'AUC': 'I', 'AUA': 'I',
+  'AUG': 'M', 'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+  'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K', 'AGU': 'S',
+  'AGC': 'S', 'AGA': 'R', 'AGG': 'R', 'GUU': 'V', 'GUC': 'V',
+  'GUA': 'V', 'GUG': 'V', 'GCU': 'A', 'GCC': 'A', 'GCA': 'A',
+  'GCG': 'A', 'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+  'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G', 'uuu': 'f',
+  'uuc': 'f', 'uua': 'l', 'uug': 'l', 'ucu': 's', 'ucc': 's',
+  'uca': 's', 'ucg': 's', 'uau': 'y', 'uac': 'y', 'uaa': '*',
+  'uag': '*', 'ugu': 'c', 'ugc': 'c', 'uga': '*', 'ugg': 'w',
+  'cuu': 'l', 'cuc': 'l', 'cua': 'l', 'cug': 'l', 'ccu': 'p',
+  'ccc': 'p', 'cca': 'p', 'ccg': 'p', 'cau': 'h', 'cac': 'h',
+  'caa': 'q', 'cag': 'q', 'cgu': 'r', 'cgc': 'r', 'cga': 'r',
+  'cgg': 'r', 'auu': 'i', 'auc': 'i', 'aua': 'i', 'aug': 'm',
+  'acu': 't', 'acc': 't', 'aca': 't', 'acg': 't', 'aau': 'n',
+  'aac': 'n', 'aaa': 'k', 'aag': 'k', 'agu': 's', 'agc': 's',
+  'aga': 'r', 'agg': 'r', 'guu': 'v', 'guc': 'v', 'gua': 'v',
+  'gug': 'v', 'gcu': 'a', 'gcc': 'a', 'gca': 'a', 'gcg': 'a',
+  'gau': 'd', 'gac': 'd', 'gaa': 'e', 'gag': 'e', 'ggu': 'g',
+  'ggc': 'g', 'gga': 'g', 'ggg': 'g'
+}
+
