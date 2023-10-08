24fde93865d354fd2e08e794099aecf04d7d23ac
Aleksandr Voskoboinikov <109810573+wwoskie@users.noreply.github.com>
2023-10-08 15:18:41 +0300

    Update imports

--- yet_another_bioinformatic_tool.py
+++ yet_another_bioinformatic_tool.py
@@ -6,6 +6,7 @@ import modules.dna_rna_tools as dna_rna_tools
 import modules.protein_tools as protein_tools
 import modules.fastq_tools as fastq_tools
 from modules.dna_rna_tools import create_input_dict
+from modules.protein_tools import read_seq_from_fasta
 
 
 command_dict_nucl = {
