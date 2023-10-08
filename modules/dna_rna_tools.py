1bcaf0ddf8b43c7a92575ac4c885fafc67e8becc
Aleksandr Voskoboinikov <109810573+wwoskie@users.noreply.github.com>
2023-10-08 09:31:11 +0300

    Update run_dna_rna_tools func

--- modules/dna_rna_tools.py
+++ modules/dna_rna_tools.py
@@ -105,23 +105,35 @@ command_dict = {
     'check_seq_type': check_seq_type,
     'reverse': reverse,
     'complement': complement,
-    'reverse_transcribe': transcribe,
     'transcribe': transcribe,
 }
 
 
 def run_dna_rna_tools(seqs: dict, command: str) -> dict:
+    '''
+    Runs dna_rna_tools on given dict of seqs with given command
+
+    Arguments:
+    - seqs (dict): Input dict of format {seq_name: 'seq'}
+
+    Return:
+    - output_dict (dict): dict of results and operations
+    '''
 
     output_dict = {}
 
     for seq_name, seq in seqs.items(): 
-        if len(seq) == 0: # dodge zero division error to a more understandable one
-            raise ValueError('Cannot work with sequence of length 0')
-        if command == 'check_seq_type':
+        if command == 'check_seq_type': # user may want to check given seqs
             output_dict |= {seq_name: check_seq_type(seq)}
-        elif isinstance(check_seq_type(seq), None):
-            raise ValueError('Can only work with DNA or RNA sequence')
-        else:
-            output_dict |= {seq_name: command_dict[command](seq)}
+        else: # if other command
+            nucl_type = check_seq_type(seq)
+            if nucl_type is None:
+                raise ValueError('Can only work with DNA or RNA sequence')
+            elif command == 'complement':
+                output_dict |= {seq_name: complement(seq, nucl_type)}
+            else:
+                output_dict |= {seq_name: command_dict[command](seq)}
 
+    if len(output_dict) == 1:
+        return output_dict[list(output_dict.keys())[0]]
     return output_dict
